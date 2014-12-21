3d57c000a4ce69b7e53e7b88370c0bde1c3f316e
Artur <oleonortt@gmail.com>
2014-12-21 18:50:12 +0200

    logout with modal dialog

--- app/viewmodels/logout.js
+++ app/viewmodels/logout.js
@@ -1,11 +1,14 @@
-define(['userContext', 'plugins/router'], function(userContext, router) {
+define(['userContext', 'plugins/router'], function (userContext, router) {
     var viewModel = {
-        canActivate: function() {
-            userContext.logout();
-            router.updateMenu(userContext.isLoggedIn());
+        canActivate: function () {
+            if (userContext.isLoggedIn()) {
+                document.querySelector('#logout').toggle();
+                return true;
+            }
             return {redirect: 'login'};
         }
     };
+
 
     return viewModel;
 });
